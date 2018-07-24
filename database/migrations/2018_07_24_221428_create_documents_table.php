7320664126c47bb538e5a3a4d8bdc49127d6777e
vagrant <vagrant@homestead>
2018-07-24 22:52:37 +0000

    main files

---
+++ database/migrations/2018_07_24_221428_create_documents_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('reference');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->unsignedInteger('document_type_id');
+            $table->unsignedInteger('document_link_type_id');
+            $table->string('link');
+            $table->string('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('documents');
+
+    }
+}
