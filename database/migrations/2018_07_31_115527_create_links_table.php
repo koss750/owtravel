7a23e6823916188717e25340ecbceb91a320fc2d
Konstantin Borodin <konstantin.borodin@lshtm.ac.uk>
2018-07-31 13:00:23 +0100

    Link model and table

---
+++ database/migrations/2018_07_31_115527_create_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('file_id');
+            $table->integer('link_type_id');
+            $table->integer('document_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
