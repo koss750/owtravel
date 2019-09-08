97e59df14bc76258e08aec18f51dc7c6ba945b4e
Konstantin Borodin <>
2019-09-08 18:08:47 +0100

    Added CLI commands to show bank cards

--- app/BankCard.php
+++ app/BankCard.php
@@ -6,4 +6,8 @@ namespace App;
 class BankCard extends BaseModel
 {
     protected $table = 'bank_cards';
+
+    protected $fillable = [
+        'ln', 'CVC', 'expiry_month', 'expiry_year', 'bank'
+    ];
 }
---
+++ app/Console/Commands/ShowBankCards.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\BankCard;
+use App\TravelProgramme;
+use App\User;
+use Illuminate\Console\Command;
+
+class ShowProgrammes extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'show:payment';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Shows user\'s bank cards';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $userQuery = $this->ask('Full name of the user');
+
+
+        $user = User::where('name', 'LIKE', '%' .$userQuery.'%')->get();
+        if ($user->count()>1) {
+            $user->toArray();
+            $this->info("Which of these users did you mean?");
+            foreach ($user as $item) {
+                $this->info("<fg=cyan> $item->id    <fg=default;bg=black>$item->name</>");
+            }
+            $userQuery = $this->ask("ID:");
+            $user = User::where('id', $userQuery)->firstOrFail();
+        }
+        else $user = User::where('name', 'LIKE', '%' .$userQuery.'%')->firstOrFail();
+
+        $specificQuery = $this->ask("Which bank? Type 'all' for all cards");
+
+        if ($specificQuery == "all") {
+
+            $items = BankCard::where('user_id', $user->id)->get();
+
+        }
+
+        else {
+
+            $items = BankCard::where([
+                ["user_id", "=", $user->id],
+                ['bank', 'LIKE', "%$specificQuery%"]
+            ])->get();
+
+        }
+
+        $headers = ['Bank', 'Number', 'Expiry', 'CVC'];
+
+        $data = array();
+
+        foreach ($items as $item) {
+
+            $data[] =
+                [
+                    'Bank' => $item->bank,
+                    'Number' => $item->ln,
+                    'Expiry' => "$item->expiry_month / $item->expiry_year",
+                    'CVC' => $item->CVC
+                ];
+
+        }
+
+        $this->table($headers, $data);
+    }
+}
