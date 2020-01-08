dec4d4c67d770403697fd6f0e999c4a4af7a0588
Konstantin Borodin <web@borodin.uk>
2020-01-08 20:13:20 +0000

    changed ew:compare to include 11pm if 10pm is strong rain

--- app/Console/Commands/CompareEWin1Hour.php
+++ app/Console/Commands/CompareEWin1Hour.php
@@ -53,26 +53,33 @@ class CompareEWin1Hour extends Command
         $chanceLater[1] = $this->controller->lineOne["chanceLater"];
         $intLater[1] = $this->controller->lineOne["intensityLater"];
 
-        $rainIndex1h = $chanceLater[0]*$intLater[0];
-        $rainIndex2h = $chanceLater[1]*$intLater[1];
+        $rainIndex1h = $chanceLater[0] * $intLater[0];
+        $rainIndex2h = $chanceLater[1] * $intLater[1];
 
         if ($rainIndex2h == $rainIndex1h && $rainIndex1h == 0) {
             $this->controller->lineOne = "Good evening! Weather update";
             $this->controller->lineTwo = "No rain at 9pm and 10pm";
-        }
-        else if ($rainIndex2h == 0) {
+        } else if ($rainIndex2h == 0) {
             $this->controller->lineOne = "Good evening! Weather update";
             $this->controller->lineTwo = "9pm - $chanceLater[0]%, int.$intLater[0]. No rain at 10pm";
-        }
-        else if ($rainIndex1h == 0) {
+        } else if ($rainIndex1h == 0) {
             $this->controller->lineOne = "Good evening! Weather update";
             $this->controller->lineTwo = "No rain at 9pm. 10pm - $chanceLater[1]%, int. $intLater[1]";
-        }
-        else {
+        } else {
             $this->controller->lineOne = "Good evening! Weather warning";
-            $this->controller->lineTwo = "9pm - $chanceLater[0]%, int.$intLater[0]. 10pm - $chanceLater[1]%, int. $intLater[1]";
+            $this->controller->lineTwo = "9pm - $chanceLater[0]%, int.$intLater[0]. 10pm - $chanceLater[1]%, int. $intLater[1]. ";
         }
 
+        if (($chanceLater[1] * $intLater[1]) > 1000) {
+            $this->controller->oneHourWeather($starting->addHour(2));
+            $chanceLater[2] = $this->controller->lineOne["chanceLater"];
+            $intLater[2] = $this->controller->lineOne["intensityLater"];
+            $rainIndex3h = $chanceLater[2] * $intLater[2];
+            $this->controller->lineOne = "Good evening! Weather warning";
+            if ($rainIndex2h > $rainIndex3h) {
+                $this->controller->lineTwo .= "11pm - $chanceLater[2]%, int. $intLater[2]";
+            }
+        }
         $this->info($this->controller->lineOne);
         $this->info($this->controller->lineTwo);
         $this->controller->action = "notification";
