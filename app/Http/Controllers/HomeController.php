8f7c3b1e38a2fd2b3cdf95e5a35c05abfe533845
Konstantin <k7501@yandex.ru>
2021-03-05 10:41:27 +0000

    use Cache; for homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
